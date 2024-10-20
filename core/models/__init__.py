1dc039b912692df10c11901f989031f8f2a1dcd4
UIE-Lab <71606804+UIE-Lab@users.noreply.github.com>
2024-10-20 15:53:14 +0300

    Create __init__.py

---
+++ core/models/__init__.py
@@ -0,0 +1,21 @@
+from .UIEC2_Net import UIEC2_Net
+from .UWCNN import UWCNN
+from .HUWIE_Net import HUWIE_Net
+from .HUWIE_Net_I2IM import HUWIE_Net_I2IM
+from .HUWIE_Net_PIM import HUWIE_Net_PIM
+from .HE import HE
+from .WB import WB
+from .UDCP import UDCP
+from .ULAP import ULAP
+
+__all__ = ['UIEC2_Net',
+           'UWCNN',
+           'HUWIE_Net',
+           'HUWIE_Net_I2IM',
+           'HUWIE_Net_PIM',
+           'HE',
+           'WB',
+           'UDCP',
+           'ULAP']
+
+
